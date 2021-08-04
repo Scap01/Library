357cc2a33e31cf85cf5df7a7200a0437b955f806
Scap <picwin12@gmail.com>
2021-08-03 22:44:42 -0300

    Base code

---
+++ LabraryMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabraryMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
