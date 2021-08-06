f08ff2b57f9d5f237550c1b521512c15435b8aa2
Scap <picwin12@gmail.com>
2021-08-06 12:07:35 -0300

    shopping modal class

--- LabraryMVC/Models/CategoryDetail.cs
+++ LabraryMVC/Models/CategoryDetail.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,37 @@ namespace LabraryMVC.Models
 {
     public class CategoryDetail
     {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category Name is Required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and maximum 100 charaters are allwed", MinimumLength = 3)]
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+
+    }
+
+    public class BookDetail
+    {
+        public int BookId { get; set; }
+        [Required(ErrorMessage = "Book Name is Required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and maximum 100 charaters are allwed", MinimumLength = 3)]
+        public string BookName { get; set; }
+        [Required]
+        [Range(1, 50)]
+        public Nullable<int> CategoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Description is Required")]
+        public string BookImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public string Author { get; set; }
+        [Required]
+        public Nullable<System.DateTime> PublicationDate { get; set; }
+        [Required]
+        public SelectList Categories { get; set; }
     }
 }
