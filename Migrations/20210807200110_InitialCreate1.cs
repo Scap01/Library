e3f445f1d76a01d93550a301725f7ecdd3783c50
Scap <picwin12@gmail.com>
2021-08-07 17:26:44 -0300

    Merge complete

---
+++ Migrations/20210807200110_InitialCreate1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Books");
+        }
+    }
+}
---
+++ Script/script-custom-datetimepicker.js
@@ -0,0 +1,9 @@
+{
+    $('#DateId').daterangepicker(
+        {
+            "singleDatePicker": true,
+            "startDate": moment(),
+            "endDate": moment().endOf('year'),
+            "opens": "center"
+        });
+}
